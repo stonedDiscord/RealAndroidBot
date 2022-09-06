0bdcfd5cc8015c2da60ae5eaf8e61ddf4f1a0b08
stonedDiscord <Tukz@gmx.de>
2022-09-06 21:23:03 +0200

    more whitespaces

--- rab/pgsharp.py
+++ rab/pgsharp.py
@@ -27,10 +27,8 @@ class PGSharp:
         self.menu_position = None
 
     async def get_nearby_count(self, p, d):
-        if d(resourceId='me.underworld.helaplugin:id/hl_sri_icon',
-                            packageName='com.nianticlabs.pokemongo').exists:
-            return d(resourceId='me.underworld.helaplugin:id/hl_sri_icon',
-                            packageName='com.nianticlabs.pokemongo').count
+        if d(resourceId='me.underworld.helaplugin:id/hl_sri_icon', packageName='com.nianticlabs.pokemongo').exists:
+            return d(resourceId='me.underworld.helaplugin:id/hl_sri_icon', packageName='com.nianticlabs.pokemongo').count
         else:
             return 0
 
@@ -39,10 +37,9 @@ class PGSharp:
         info1 = None
         info2 = None
         info3 = None  # this is for timer
-        pokemon_info = d(resourceId='com.nianticlabs.pokemongo:id/unitySurfaceView',
-                        packageName='com.nianticlabs.pokemongo').info
+        pokemon_info = d(resourceId='com.nianticlabs.pokemongo:id/unitySurfaceView', packageName='com.nianticlabs.pokemongo').info
         cd_timer = False
-        
+
         height = 1920
         if pokemon_info['bounds'].get('bottom') > 1280:
             height = 1920
@@ -50,10 +47,10 @@ class PGSharp:
         else:
             height = 1280
             width = 720
-        
+
         # 0 = menu
         # 1 = joystick
-        # 2 = feed 
+        # 2 = feed
         # total handle count
         floating_icon_count = d(className='android.widget.FrameLayout', packageName='com.nianticlabs.pokemongo',clickable=True).count
         if floating_icon_count == 2:
@@ -62,11 +59,11 @@ class PGSharp:
                 cd_timer = True
             info0 = d(className='android.widget.FrameLayout', packageName='com.nianticlabs.pokemongo',clickable=True)[0].info # menu
             info2 = d(className='android.widget.FrameLayout', packageName='com.nianticlabs.pokemongo',clickable=True)[1].info # Feed
-             
+
         elif floating_icon_count == 3:
             if d(resourceId='me.underworld.helaplugin:id/hl_cd_text', packageName='com.nianticlabs.pokemongo').exists:
                 cd_timer = True
-            
+
             if not cd_timer:
                 info0 = d(className='android.widget.FrameLayout', packageName='com.nianticlabs.pokemongo',clickable=True)[0].info # menu
                 info1 = d(className='android.widget.FrameLayout', packageName='com.nianticlabs.pokemongo',clickable=True)[1].info # joystick
@@ -77,7 +74,7 @@ class PGSharp:
                 info3 = d(className='android.widget.FrameLayout', packageName='com.nianticlabs.pokemongo',clickable=True)[2].info # Timer
         else:
             return False
-        
+
         # Move Feed First
         feed_sx, feed_sy = await self.get_item_position(info2)
         feed_ex, feed_ey = 0.94 * width, 0.90 * height
@@ -91,14 +88,14 @@ class PGSharp:
             feed_ex, feed_ey = 0, 0.98 * height
             d.drag(feed_sx, feed_sy, feed_ex, feed_ey, 1)
             await p.tap(feed_ex+25, feed_ey)  # This will prevent icons from moving
-        
+
         # Move timer
         if info1 and floating_icon_count == 3:
             info3 = d(className='android.widget.FrameLayout', packageName='com.nianticlabs.pokemongo',clickable=True)[2].info  # Timer
             feed_sx, feed_sy = await self.get_item_position(info3)
             feed_ex, feed_ey = 0.5 * width, 0
             d.drag(feed_sx, feed_sy, feed_ex, feed_ey, 1)
-        
+
         # Move menu icon
         info0 = d(className='android.widget.FrameLayout', packageName='com.nianticlabs.pokemongo',clickable=True)[0].info  # menu
         feed_sx, feed_sy = await self.get_item_position(info0)
@@ -108,8 +105,8 @@ class PGSharp:
         if d(resourceId='me.underworld.helaplugin:id/hl_floatmenu_map', packageName='com.nianticlabs.pokemongo').exists:
             # tap one more time to close it
             await p.tap(feed_ex, feed_ey)
-        #await asyncio.sleep(4.0)
-        
+        # await asyncio.sleep(4.0)
+
         if floating_icon_count == 3 and cd_timer:
             info2 = d(className='android.widget.FrameLayout', packageName='com.nianticlabs.pokemongo',clickable=True)[1].info  # Feed
         elif floating_icon_count == 3 and not cd_timer:
@@ -129,16 +126,16 @@ class PGSharp:
         this_feed_index = None
         this_joystick_index = None
         this_icon_index = None
-        
+
         floating_icon_count = d(resourceId='me.underworld.helaplugin:id/hl_floating_icon', packageName='com.nianticlabs.pokemongo').count
         pokemon_info = d(resourceId='com.nianticlabs.pokemongo:id/unitySurfaceView', packageName='com.nianticlabs.pokemongo').info
-        
+
         height = 1920
         if pokemon_info['bounds'].get('bottom') > 1280:
             height = 1920
         else:
             height = 1280
-        
+
         if floating_icon_count == 3:
             # verify who is feed
             for x in range(3):
@@ -158,7 +155,7 @@ class PGSharp:
                         break
                 except:
                     pass
-            
+
             # verify who is icon
             rem_diff = 0
             rem_this_icon_index = 0 
@@ -176,9 +173,9 @@ class PGSharp:
                         rem_this_icon_index = x
                 except:
                     pass
-            
+
             this_icon_index = rem_this_icon_index
-            
+
             # verify who is joystick
             for x in range(3):
                 if this_feed_index == x:
@@ -186,15 +183,15 @@ class PGSharp:
                 if this_icon_index == x:
                     continue
                 this_joystick_index = x
-            
+
             logger.info(f'Feed Index: {this_feed_index} | Icon Index: {this_icon_index} | Joystick Index: {this_joystick_index}')    
-            
+
             feed_info = d(resourceId='me.underworld.helaplugin:id/hl_floating_icon', packageName='com.nianticlabs.pokemongo')[this_feed_index].info
             joystick_info = d(resourceId='me.underworld.helaplugin:id/hl_floating_icon', packageName='com.nianticlabs.pokemongo')[this_joystick_index].info
             icon_info = d(resourceId='me.underworld.helaplugin:id/hl_floating_icon', packageName='com.nianticlabs.pokemongo')[this_icon_index].info
-            
+
             feed_shifted = False
-            
+
             feed_sx, feed_sy = await self.get_item_position(feed_info)
             if (feed_info['bounds'].get('top') == joystick_info['bounds'].get('top') and feed_info['bounds'].get('left') == joystick_info['bounds'].get('left')) or (feed_info['bounds'].get('top') == icon_info['bounds'].get('top') and feed_info['bounds'].get('left') == icon_info['bounds'].get('left')):
                 feed_ex, feed_ey = 0, 0.5 * height
@@ -202,17 +199,17 @@ class PGSharp:
             else:
                 feed_ex, feed_ey = 0, 0.2 * height
             d.drag(feed_sx, feed_sy, feed_ex, feed_ey, 0.5)
-        
+
             # Move joystick
             joystick_sx, joystick_sy = await self.get_item_position(joystick_info)
             joystick_ex, joystick_ey = 0, 0.99 * height
             d.drag(joystick_sx, joystick_sy, joystick_ex, joystick_ey, 0.5)
-        
+
             # Move icon
             icon_sx, icon_sy = await self.get_item_position(icon_info)
             icon_ex, icon_ey = 0.92 * pokemon_info['bounds'].get('right'), 0.35 * height
             d.drag(icon_sx, icon_sy, icon_ex, icon_ey)
-            
+
             this_feed_index = None
             this_joystick_index = None
             this_icon_index = None
@@ -234,10 +231,10 @@ class PGSharp:
                         break
                 except:
                     pass
-        
+
             # verify who is icon
             rem_diff = 0
-            rem_this_icon_index = 0 
+            rem_this_icon_index = 0
             for x in range(3):
                 try:
                     # skip the index that is confirm feed
@@ -252,9 +249,9 @@ class PGSharp:
                         rem_this_icon_index = x
                 except:
                     pass
-            
+
             this_icon_index = rem_this_icon_index
-            
+
             # verify who is joystick
             for x in range(3):
                 if this_feed_index == x:
@@ -262,45 +259,45 @@ class PGSharp:
                 if this_icon_index == x:
                     continue
                 this_joystick_index = x
-            
+
             logger.info(f'Feed Index: {this_feed_index} | Icon Index: {this_icon_index} | Joystick Index: {this_joystick_index}')
-            
+
             # Move feed
             if feed_shifted:
                 feed_info = d(resourceId='me.underworld.helaplugin:id/hl_floating_icon', packageName='com.nianticlabs.pokemongo')[this_feed_index].info
                 feed_sx, feed_sy = await self.get_item_position(feed_info)
                 feed_ex, feed_ey = 0, 0.2 * height
                 d.drag(feed_sx, feed_sy, feed_ex, feed_ey, 0.5)
-            
+
             self.feed_index = this_feed_index
             self.joystick_index = this_joystick_index
             self.icon_index = this_icon_index
-            
+
             await asyncio.sleep(6.0)
-            x,y = await self.get_location(p, d)
-            if x>0:
-                self.start_location = [x,y]
+            x, y = await self.get_location(p, d)
+            if x > 0:
+                self.start_location = [x, y]
                 logger.info('RAB is able to retrieve location from PGSharp...')
             else:
                 logger.info('RAB is unable to retrieve location from PGSharp...')
         else:
             return False
-    
+
     async def get_item_position(self, info, resized=False):
         ya = info['bounds'].get('top')
         xs = info['bounds'].get('left')
         ya1 = info['bounds'].get('bottom')
         xs1 = info['bounds'].get('right')
-        
+
         x = xs + int(((xs1 - xs)/2))
         y = ya + int(((ya1 - ya)/2))
-        
+
         if resized:
             x = x * 2
             y = y * 2
-        
+
         return (x, y)
-        
+
     async def wait_for_spawn(self, p, d):
         counter = 0
         while True:
@@ -310,7 +307,7 @@ class PGSharp:
             if d(resourceId='me.underworld.helaplugin:id/hl_sri_icon', packageName='com.nianticlabs.pokemongo').exists:
                 return True
             counter += 1
-    
+
     async def teleport(self, p, d, x, y, resized=False):
         iconx = 0
         icony = 0
@@ -323,7 +320,7 @@ class PGSharp:
         #    if not d(resourceId='me.underworld.helaplugin:id/hl_floatmenu_map', packageName='com.nianticlabs.pokemongo').exists:
         #        d(className='android.widget.FrameLayout', packageName='com.nianticlabs.pokemongo',clickable=True)[self.menu_index].click()
                 #await p.tap(iconx,icony)
-        
+
         # Open map icon
         #if d(resourceId='me.underworld.helaplugin:id/hl_floating_icon', packageName='com.nianticlabs.pokemongo').exists:
         #    info = d(resourceId='me.underworld.helaplugin:id/hl_floating_icon', packageName='com.nianticlabs.pokemongo')[self.icon_index].info
@@ -331,15 +328,15 @@ class PGSharp:
         #    if not d(resourceId='me.underworld.helaplugin:id/hl_floatmenu_map', packageName='com.nianticlabs.pokemongo').exists:
                 #d(className='android.widget.FrameLayout', packageName='com.nianticlabs.pokemongo',clickable=True)[self.menu_index].click()
         #        await p.tap(iconx,icony)
-        
+
         if not d(resourceId='me.underworld.helaplugin:id/hl_floatmenu_map', packageName='com.nianticlabs.pokemongo').exists:
             d(className='android.widget.FrameLayout', packageName='com.nianticlabs.pokemongo',clickable=True)[self.menu_index].click()
-        
+
         # Open Teleport
         if d(resourceId='me.underworld.helaplugin:id/hl_floatmenu_tp', packageName='com.nianticlabs.pokemongo').exists:
             d(resourceId='me.underworld.helaplugin:id/hl_floatmenu_tp', packageName='com.nianticlabs.pokemongo').click()
             await asyncio.sleep(0.5)
-        
+
         text = str(x) + ', ' + str(y)
         try:
             d.set_fastinput_ime(True) 
@@ -348,12 +345,12 @@ class PGSharp:
             d.set_fastinput_ime(False)
         except:
             d(focused=True).set_text(text)
-            
+
         await asyncio.sleep(1)
         d(text='OK', resourceId='android:id/button1', packageName='com.nianticlabs.pokemongo').click()
-        
+
         await asyncio.sleep(0.5)
-        
+
         if iconx > 0 and d(resourceId='me.underworld.helaplugin:id/hl_floatmenu_map', packageName='com.nianticlabs.pokemongo').exists:
             d(className='android.widget.FrameLayout', packageName='com.nianticlabs.pokemongo',clickable=True)[self.menu_index].click()
             #await p.tap(iconx,icony)
