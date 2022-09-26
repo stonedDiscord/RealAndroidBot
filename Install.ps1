2f0ecca880d8c3b16670baae98c6c566df53d198
stonedDiscord <Tukz@gmx.de>
2022-09-26 21:10:30 +0200

    question is no longer needed

--- Install.ps1
+++ Install.ps1
@@ -2,15 +2,6 @@ if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]:
 
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 
-cup -y python tesseract
+cup -y python tesseract adb
 
 pip install -r requirements.txt
-
-$title    = 'Device Type'
-$question = 'What kind of device do you want to control?'
-$choices  = '&Real Phone', '&Emulator'
-
-$decision = $Host.UI.PromptForChoice($title, $question, $choices, 0)
-if ($decision -eq 0) {
-    cup -y adb
-}
