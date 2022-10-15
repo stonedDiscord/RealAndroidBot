545a8978fc5069d859bfd04745241553843214b1
stonedDiscord <Tukz@gmx.de>
2022-10-15 20:55:50 +0200

    install arch deps

--- install.sh
+++ install.sh
@@ -1,1 +1,4 @@
-sudo apt-get install adb python3-pil.imagetk python3-pip python3-tk
+sudo apt-get install adb python3-pil.imagetk python3-pip python3-tk tesseract-ocr
+pacman -S android-tools tk python-pytesseract tesseract-data-eng
+
+pip3 install -r requirements.txt
