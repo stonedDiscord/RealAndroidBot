394d381eefc48cf78483ad895d99f6164743e21a
stonedDiscord <Tukz@gmx.de>
2022-10-15 21:00:49 +0200

    change CI to use included script

--- install.sh
+++ install.sh
@@ -2,4 +2,6 @@ sudo apt-get install adb python3-pil.imagetk python3-pip python3-tk tesseract-oc
 sudo pacman -Sy
 sudo pacman -S android-tools tk python-pip python-pytesseract tesseract-data-eng
 
+python3 -m pip install --upgrade pip
+
 pip3 install -r requirements.txt
